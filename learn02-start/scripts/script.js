9738167894bee4bea347adcf7b0c3d0ca33d091e
Kaitlyn Clay <kclay@students.mchenry.edu>
2024-02-12 21:55:08 -0600

    initial commit

---
+++ learn02-start/scripts/script.js
@@ -0,0 +1,11 @@
+function myFunction2() {
+    document.getElementById("sample").innerHTML =
+        "Changed from a function in the External Script.";
+}
+    
+// single line commnet
+
+/*
+Block Comments /
+Multiline Comments
+*/
